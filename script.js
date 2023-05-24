89c879b597f52b1e89e631604acc0c33640c2079
kadirmalik787 <kadirmalik457@gmail.com>
2023-05-24 07:43:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+ const scanner = new Html5QrcodeScanner("reader",{
+  qrbox:{
+   width:250,
+   height:250,
+  },
+  fps:20,
+ });
+
+ scanner.render(success,error);
+
+ function success(result){
+
+    console.log(result)
+  document.getElementById('result').innerHTML=`
+  <h2>success!</h2>
+  <p><a href="${result}">${result}</a></p>`
+
+  scanner.clear()
+  document.getElementById("reader").remove();
+ }
+
+ function error(error){
+  console.log(error)
+ }
